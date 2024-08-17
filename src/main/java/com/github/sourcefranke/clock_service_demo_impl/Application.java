8dcf021cf99e497b6ce12f3759e4f951be1424f8
Lukas Tietenberg <lukas.tietenberg@outlook.de>
2024-08-17 16:09:03 +0200

    wip

---
+++ src/main/java/com/github/sourcefranke/clock_service_demo_impl/Application.java
@@ -0,0 +1,13 @@
+package com.github.sourcefranke.clock_service_demo_impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
