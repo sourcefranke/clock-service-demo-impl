4823d75cd55a65bcc7adb0a333844f615c1fb287
Lukas Tietenberg <lukas.tietenberg@outlook.de>
2024-08-18 20:17:06 +0200

    wip

--- src/main/java/com/github/sourcefranke/clock_service_demo_impl/TimeApiDelegateImpl.java
+++ src/main/java/com/github/sourcefranke/clock_service_demo_impl/TimeApiDelegateImpl.java
@@ -2,6 +2,7 @@ package com.github.sourcefranke.clock_service_demo_impl;
 
 import com.github.sourcefranke.clock_service_demo_impl.api.TimeApiDelegate;
 import com.github.sourcefranke.clock_service_demo_impl.model.GetTime200Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +11,17 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @Component
+@Slf4j
 public class TimeApiDelegateImpl implements TimeApiDelegate {
 
     @Override
     public ResponseEntity<GetTime200Response> getTime(String dateFormat, String timeZone) {
-        return ResponseEntity.ok(
-                new GetTime200Response()
-                        .time(LocalDateTime.now(ZoneId.of(timeZone)).format(DateTimeFormatter.ofPattern(dateFormat)))
-                        .dateFormat(dateFormat)
-                        .timeZone(timeZone)
-        );
+        log.debug("getTime, parameters: dateFormat [{}], timeZone [{}]", dateFormat, timeZone);
+        var response = new GetTime200Response()
+                .time(LocalDateTime.now(ZoneId.of(timeZone)).format(DateTimeFormatter.ofPattern(dateFormat)))
+                .dateFormat(dateFormat)
+                .timeZone(timeZone);
+        log.info("GET /time?date-format={}&time-zone={}, Response: {}", dateFormat, timeZone, response);
+        return ResponseEntity.ok(response);
     }
 }
